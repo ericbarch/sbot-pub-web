e6bc38d6360847e86ac557d7d5c79092942ea78e
Eric Barch <ericb@ericbarch.com>
2018-10-28 14:59:46 -0400

    Do not re-create sbot instance if we already have one.

--- index.js
+++ index.js
@@ -5,22 +5,30 @@ const SERVER_PORT = 3000;
 const app = express();
 app.set('view engine', 'ejs');
 
+let cachedSbotInstance = null;
+
 app.get('/', async (req, res) => {
   try {
     const sbot = await getSbotInstance();
     const inviteCode = await getInvite(sbot);
     res.render('index', { inviteCode: inviteCode });
   } catch (sbotErr) {
+    cachedSbotInstance = null;
     res.sendStatus(500);
   }
 });
 
 function getSbotInstance () {
   return new Promise((resolve, reject) => {
+    if (cachedSbotInstance) {
+      return resolve(cachedSbotInstance);
+    }
+
     ssbClient((err, sbot) => {
       if (err) {
         reject(err);
       } else {
+        cachedSbotInstance = sbot;
         resolve(sbot);
       }
     });
